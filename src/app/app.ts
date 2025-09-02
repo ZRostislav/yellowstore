9c65adf45d2c1bb29413d0d590d15ddb39d5a5f3
ZRostislav <mrsnerak0@gmail.com>
2025-09-02 11:06:33 +0300

    v0.0.2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from './core/top-bar/top-bar.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopBarComponent, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
