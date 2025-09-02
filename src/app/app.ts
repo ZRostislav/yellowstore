f2ae4c39cf2d985767949b540a5f358f12e56653
ZRostislav <mrsnerak0@gmail.com>
2025-09-02 18:06:04 +0300

    v0.0.3

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBarComponent } from './core/top-bar/top-bar.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopBarComponent, HeaderComponent],
+  imports: [RouterOutlet, TopBarComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
